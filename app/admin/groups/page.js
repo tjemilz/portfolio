756506910d2f2cfb59a7052d4ca07bff83d8de13
tjemilz <efourgnier94370@gmail.com>
2025-12-07 12:13:06 +0100

    fix: remove duplicate buildApiUrl import

--- app/admin/groups/page.js
+++ app/admin/groups/page.js
@@ -3,7 +3,6 @@
 import React, { useState, useEffect } from 'react';
 import AdminLayout from '../AdminLayout';
 import { buildApiUrl } from '@/app/lib/apiUtils';
-import { buildApiUrl } from '@/app/lib/apiUtils';
 
 const DEFAULT_GROUPS = [
   { name: 'Famille', description: 'Membres de la famille' },
